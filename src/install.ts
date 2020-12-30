e8868159d7d13052bcb46228ea6a6549a8987adf
Daniel Flanagan <daniel@lyte.dev>
2020-12-30 14:07:20 -0600

    Use correct path logic

--- src/install.ts
+++ src/install.ts
@@ -194,7 +194,7 @@ fi`
       fs.writeFileSync(shellInfo.path, 'export PATH=${0:A:h}/bin:$PATH')
       return writeConfig('.zshrc', initContent)
     case 'fish':
-      fs.writeFileSync(shellInfo.path, 'set -U fish_user_paths /usr/local/bin $fish_user_paths')
+      fs.writeFileSync(shellInfo.path, 'set -U fish_user_paths (dirname (status --current-filename)) $fish_user_paths')
       return writeConfig('.config/fish/config.fish', `test -f '${shellInfo.path}' && source '${shellInfo.path}'`)
     default:
       const error = `Unable to set credential helper in PATH. We don't how to set the path for ${shellInfo.shell} shell.
